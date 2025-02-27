23022f3685774557e529d6ef43298eae07cccf48
Udara Lakshan <ulakshan061@gmail.com>
2025-02-27 22:28:46 +0530

    Implement routing and layout for Menu and Customize pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,30 @@
+import { Route, Routes } from "react-router-dom"
+import MenuPage from "./pages/MenuPage"
+import Navbar from "./components/Navbar"
+import CustomizePage from "./pages/CustomizePage"
+
 function App() {
-  return (
-    <>
-    </>
-  )
+	return (
+		<div className="flex justify-center overflow-hidden">
+			<div className="max-w-screen-xl h-screen flex items-center flex-col">
+				<Navbar />
+				<Routes>
+					{["/", "/coffee", "/shakes", "/tea", "/bubble-tea"].map(
+						(path) => (
+							<Route
+								path={path}
+								element={<MenuPage />}
+							/>
+						)
+					)}
+					<Route
+						path="/customize"
+						element={<CustomizePage />}
+					/>
+				</Routes>
+			</div>
+		</div>
+	)
 }
 
 export default App
