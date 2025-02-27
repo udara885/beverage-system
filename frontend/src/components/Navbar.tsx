798ed904217d90aed92c6ebd5b738d1f85594d4a
Udara Lakshan <ulakshan061@gmail.com>
2025-02-27 22:29:24 +0530

    Add Navbar component for navigation and layout integration

---
+++ frontend/src/components/Navbar.tsx
@@ -0,0 +1,19 @@
+import { useLocation } from "react-router-dom"
+import BackButton from "./BackButton"
+import Logo from "./Logo"
+import Menu from "./Menu"
+
+const Navbar = () =>
+{
+  const location = useLocation()
+  const path = location.pathname
+  return (
+    <div className="flex items-center justify-between my-5 px-10 w-screen">
+      <BackButton />
+      {path !== "/customize" && <Menu />}
+      <Logo/>
+    </div>
+  )
+}
+
+export default Navbar
