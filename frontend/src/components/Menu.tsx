591236ac32929cac6d37bf78bbc9e0348da80c95
Udara Lakshan <ulakshan061@gmail.com>
2025-02-27 22:29:14 +0530

    Add Menu component for beverage navigation

---
+++ frontend/src/components/Menu.tsx
@@ -0,0 +1,34 @@
+import { Link } from "react-router-dom"
+
+const Menu = () => {
+	return (
+		<div className="font-poppins flex justify-between text-[28px] font-medium gap-10 whitespace-nowrap">
+			<Link
+				to={"/coffee"}
+				className="border-b-3 border-transparent focus:border-orange-400 cursor-pointer"
+			>
+				Coffee
+			</Link>
+			<Link
+				to={"/shakes"}
+				className="border-b-3 border-transparent focus:border-orange-400 cursor-pointer"
+			>
+				Shakes
+			</Link>
+			<Link
+				to={"/tea"}
+				className="border-b-3 border-transparent focus:border-orange-400 cursor-pointer"
+			>
+				Tea
+			</Link>
+			<Link
+				to={"/bubble-tea"}
+				className="border-b-3 border-transparent focus:border-orange-400 cursor-pointer"
+			>
+				Bubble Tea
+			</Link>
+		</div>
+	)
+}
+
+export default Menu
