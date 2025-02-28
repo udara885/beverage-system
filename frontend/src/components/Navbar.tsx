fa0ca21f5ba944047a7b6712bdb94bae8399607c
Udara Lakshan <ulakshan061@gmail.com>
2025-02-28 19:03:20 +0530

    Adjust Navbar padding for improved layout and spacing

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -3,17 +3,16 @@ import BackButton from "./BackButton"
 import Logo from "./Logo"
 import Menu from "./Menu"
 
-const Navbar = () =>
-{
-  const location = useLocation()
-  const path = location.pathname
-  return (
-    <div className="flex items-center justify-between my-5 px-10 w-screen">
-      <BackButton />
-      {path !== "/customize" && <Menu />}
-      <Logo/>
-    </div>
-  )
+const Navbar = () => {
+	const location = useLocation()
+	const path = location.pathname
+	return (
+		<div className="flex  justify-between my-5 px-20 w-screen items-center">
+			<BackButton />
+			{path !== "/customize" && <Menu />}
+			<Logo />
+		</div>
+	)
 }
 
 export default Navbar
