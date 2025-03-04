9420a79cea61f3e0980327f65d2f21c317a8a056
Udara Lakshan <ulakshan061@gmail.com>
2025-03-04 18:34:11 +0530

    Refactor CartCard component for improved layout and conditional rendering based on path

--- frontend/src/components/CartCard.tsx
+++ frontend/src/components/CartCard.tsx
@@ -1,32 +1,42 @@
 import { Settings, Trash } from "lucide-react"
+import { useLocation } from "react-router-dom"
 
 const CartCard = () => {
-	return (
-		<div className="rounded-lg flex items-center justify-around shadow-[0px_4px_30.8px_0px_rgba(0,0,0,0.25)] w-full">
-			<img
-				src="cartCard.png"
-				className="w-40 h-40 rounded-full my-3"
-				alt=""
-			/>
-			<div className="font-poppins">
-				<h2 className="text-2xl font-semibold">Berry Blast Smoothie</h2>
-				<h3 className="text-lg font-medium">customized - Whole Milk</h3>
-				<div className="border-2 rounded-lg w-1/2 flex items-center justify-around mt-5">
-					<button className="cursor-pointer">-</button>
-					<span className="font-bold">1</span>
-					<button className="cursor-pointer">+</button>
-				</div>
-			</div>
-			<div className="flex flex-col gap-2 self-end pb-5">
-				<button className="bg-orange-400 text-white p-1 rounded-lg cursor-pointer">
-					<Trash />
-				</button>
-				<button className="bg-orange-400 text-white p-1 rounded-lg cursor-pointer">
-					<Settings />
-				</button>
-			</div>
-		</div>
-	)
+  const location = useLocation()
+  const path = location.pathname
+  return (
+    <div className="flex w-full items-center justify-around rounded-lg shadow-[0px_4px_30.8px_0px_rgba(0,0,0,0.25)] px-6 gap-5">
+      <img src="cartCard.png" className="my-3 h-40 w-40 rounded-full" alt="" />
+      <div className="font-poppins">
+        <h2 className="text-2xl font-semibold">Berry Blast Smoothie</h2>
+        {path === "/cart" ? (
+          <>
+            <h3 className="text-lg font-medium">customized - Whole Milk</h3>
+            <div className="mt-5 flex w-1/2 items-center justify-around rounded-lg border-2">
+              <button className="cursor-pointer">-</button>
+              <span className="font-bold">1</span>
+              <button className="cursor-pointer">+</button>
+            </div>
+          </>
+        ) : (
+          <p className="text-lg text-justify">
+            A refreshing blend of strawberries, blueberries, and raspberries
+            mixed with yogurt and a splash of apple juice for a fruity delight.
+          </p>
+        )}
+      </div>
+      {path === "/cart" && (
+        <div className="flex flex-col gap-2 self-end pb-5">
+          <button className="cursor-pointer rounded-lg bg-orange-400 p-1 text-white">
+            <Trash />
+          </button>
+          <button className="cursor-pointer rounded-lg bg-orange-400 p-1 text-white">
+            <Settings />
+          </button>
+        </div>
+      )}
+    </div>
+  )
 }
 
 export default CartCard
