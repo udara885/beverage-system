8f5b849d855995c368e04da29471a9215b85f5e6
Udara Lakshan <ulakshan061@gmail.com>
2025-03-04 18:34:06 +0530

    Refactor BeverageCard component for improved readability and consistent formatting

--- frontend/src/components/BeverageCard.tsx
+++ frontend/src/components/BeverageCard.tsx
@@ -1,32 +1,32 @@
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
 
 const BeverageCard = () => {
   return (
-		<div>
-			<hr />
-			<div className="flex mt-5 gap-5">
-				<img
-					src="espresso.png"
-					alt=""
-					className="w-30 h-30 rounded-full object-cover bg-no-repeat"
-					loading="lazy"
-				/>
-				<div className="flex flex-col font-poppins">
-					<h1 className="text-[1.75rem] font-semibold">
-						Espresso - $3.00
-					</h1>
-					<p className="text-[1.375rem]">
-						A rich, concentrated coffee brewed by forcing a small
-						amount of nearly boiling water through finely-ground
-						coffee beans.
-					</p>
-					<Link to={"/customize"} className="bg-orange-400 font-semibold text-white rounded-lg px-3 text-center text-lg self-end mx-3 mb-3">
-						{"Customize >"}
-					</Link>
-				</div>
-			</div>
-		</div>
-  )
-}
+    <div>
+      <hr />
+      <div className="mt-5 flex gap-5">
+        <img
+          src="espresso.png"
+          alt=""
+          className="h-30 w-30 rounded-full bg-no-repeat object-cover"
+          loading="lazy"
+        />
+        <div className="font-poppins flex flex-col">
+          <h1 className="text-[1.75rem] font-semibold">Espresso - $3.00</h1>
+          <p className="text-[1.375rem]">
+            A rich, concentrated coffee brewed by forcing a small amount of
+            nearly boiling water through finely-ground coffee beans.
+          </p>
+          <Link
+            to={"/customize"}
+            className="mx-3 mb-3 self-end rounded-lg bg-orange-400 px-3 text-center text-lg font-semibold text-white"
+          >
+            {"Customize >"}
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default BeverageCard
+export default BeverageCard;
