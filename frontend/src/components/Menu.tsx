ab11afa7b7b64117a59dc3c09ace0ea8eddb4e95
Udara Lakshan <ulakshan061@gmail.com>
2025-03-04 18:34:22 +0530

    Refactor Menu component for improved readability and consistent formatting

--- frontend/src/components/Menu.tsx
+++ frontend/src/components/Menu.tsx
@@ -1,34 +1,34 @@
 import { Link } from "react-router-dom"
 
 const Menu = () => {
-	return (
-		<div className="font-poppins flex justify-between items-center text-[28px] font-medium gap-10 whitespace-nowrap">
-			<Link
-				to={"/coffee"}
-				className="border-b-3 border-transparent focus:border-orange-400 cursor-pointer"
-			>
-				Coffee
-			</Link>
-			<Link
-				to={"/shakes"}
-				className="border-b-3 border-transparent focus:border-orange-400 cursor-pointer"
-			>
-				Shakes
-			</Link>
-			<Link
-				to={"/tea"}
-				className="border-b-3 border-transparent focus:border-orange-400 cursor-pointer"
-			>
-				Tea
-			</Link>
-			<Link
-				to={"/bubble-tea"}
-				className="border-b-3 border-transparent focus:border-orange-400 cursor-pointer"
-			>
-				Bubble Tea
-			</Link>
-		</div>
-	)
+  return (
+    <div className="font-poppins flex items-center justify-between gap-10 text-[28px] font-medium whitespace-nowrap">
+      <Link
+        to={"/coffee"}
+        className="cursor-pointer border-b-3 border-transparent focus:border-orange-400"
+      >
+        Coffee
+      </Link>
+      <Link
+        to={"/shakes"}
+        className="cursor-pointer border-b-3 border-transparent focus:border-orange-400"
+      >
+        Shakes
+      </Link>
+      <Link
+        to={"/tea"}
+        className="cursor-pointer border-b-3 border-transparent focus:border-orange-400"
+      >
+        Tea
+      </Link>
+      <Link
+        to={"/bubble-tea"}
+        className="cursor-pointer border-b-3 border-transparent focus:border-orange-400"
+      >
+        Bubble Tea
+      </Link>
+    </div>
+  )
 }
 
 export default Menu
