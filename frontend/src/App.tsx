79b56e21fe89ed9ea708e8712f4abe1c41329d23
Udara Lakshan <ulakshan061@gmail.com>
2025-03-04 18:33:43 +0530

    Add new pages for Card Details, Order Confirmation, and Feedback to App component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,38 +4,31 @@ import Navbar from "./components/Navbar"
 import CustomizePage from "./pages/CustomizePage"
 import CartPage from "./pages/CartPage"
 import CheckoutPage from "./pages/CheckoutPage"
+import CardDetailsPage from "./pages/CardDetailsPage"
+import OrderConfirmPage from "./pages/OrderConfirmPage"
+import FeedbackPage from "./pages/FeedbackPage"
 
 function App() {
-	return (
-		<div className="flex justify-center overflow-hidden">
-			<div className="max-w-screen-xl h-screen flex items-center flex-col">
-				<Navbar />
-				<Routes>
-					{["/", "/coffee", "/shakes", "/tea", "/bubble-tea"].map(
-						(path, index) => (
-							<Route
-								key={index}
-								path={path}
-								element={<MenuPage />}
-							/>
-						)
-					)}
-					<Route
-						path="/customize"
-						element={<CustomizePage />}
-					/>
-					<Route
-						path="/cart"
-						element={<CartPage />}
-					/>
-					<Route
-						path="/checkout"
-						element={<CheckoutPage />}
-					/>
-				</Routes>
-			</div>
-		</div>
-	)
+  return (
+    <div className="flex justify-center overflow-hidden">
+      <div className="flex h-screen max-w-screen-xl flex-col items-center">
+        <Navbar />
+        <Routes>
+          {["/", "/coffee", "/shakes", "/tea", "/bubble-tea"].map(
+            (path, index) => (
+              <Route key={index} path={path} element={<MenuPage />} />
+            ),
+          )}
+          <Route path="/customize" element={<CustomizePage />} />
+          <Route path="/cart" element={<CartPage />} />
+          <Route path="/checkout" element={<CheckoutPage />} />
+          <Route path="/card-details" element={<CardDetailsPage />} />
+          <Route path="/order-confirm" element={<OrderConfirmPage />} />
+          <Route path="/feedback" element={<FeedbackPage />} />
+        </Routes>
+      </div>
+    </div>
+  )
 }
 
 export default App
