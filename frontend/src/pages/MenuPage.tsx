338739ea793b07fd7c29b43baccca197b182ac2f
Udara Lakshan <ulakshan061@gmail.com>
2025-03-04 18:35:02 +0530

    Refactor MenuPage component for improved readability and consistent formatting

--- frontend/src/pages/MenuPage.tsx
+++ frontend/src/pages/MenuPage.tsx
@@ -1,43 +1,43 @@
-import { useLocation } from "react-router-dom"
-import MenuContent from "../components/MenuContent"
+import { useLocation } from "react-router-dom";
+import MenuContent from "../components/MenuContent";
 
 const MenuPage = () => {
-	const location = useLocation()
-	const path = location.pathname
+  const location = useLocation();
+  const path = location.pathname;
 
-	return (
-		<div>
-			<h1 className="font-poppins text-5xl font-medium my-6 mx-15">
-				{path === "/coffee" || path === "/"
-					? "Coffee"
-					: path === "/shakes"
-					? "Shakes"
-					: path === "/tea"
-					? "Tea"
-					: "Bubble Tea"}
-			</h1>
-			<div className="flex justify-between mx-8">
-				<div className="w-2/3 h-10">
-					<MenuContent />
-				</div>
-				<div className="w-1/3">
-					<img
-						src={
-							path === "/coffee" || path === "/"
-								? "coffee.png"
-								: path === "/shakes"
-								? "shake.png"
-								: path === "/tea"
-								? "tea.png"
-								: "bubble-tea.png"
-						}
-						alt=""
-						className="object-cover bg-no-repeat -mt-31"
-					/>
-				</div>
-			</div>
-		</div>
-	)
-}
+  return (
+    <div>
+      <h1 className="font-poppins mx-15 my-6 text-5xl font-medium">
+        {path === "/coffee" || path === "/"
+          ? "Coffee"
+          : path === "/shakes"
+            ? "Shakes"
+            : path === "/tea"
+              ? "Tea"
+              : "Bubble Tea"}
+      </h1>
+      <div className="mx-8 flex justify-between">
+        <div className="h-10 w-2/3">
+          <MenuContent />
+        </div>
+        <div className="w-1/3">
+          <img
+            src={
+              path === "/coffee" || path === "/"
+                ? "coffee.png"
+                : path === "/shakes"
+                  ? "shake.png"
+                  : path === "/tea"
+                    ? "tea.png"
+                    : "bubble-tea.png"
+            }
+            alt=""
+            className="-mt-31 bg-no-repeat object-cover"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default MenuPage
+export default MenuPage;
