edd735aad44345dd713464f756d22f7374bace0d
Udara Lakshan <ulakshan061@gmail.com>
2025-03-04 18:34:32 +0530

    Refactor Navbar component for improved layout and conditional rendering based on path

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -4,15 +4,26 @@ import Logo from "./Logo"
 import Menu from "./Menu"
 
 const Navbar = () => {
-	const location = useLocation()
-	const path = location.pathname
-	return (
-		<div className="flex justify-between my-5 w-[80rem] items-center">
-			<BackButton />
-			{path !== "/customize" && path !== "/cart" && path !== "/checkout" ? <Menu /> : <></>}
-			<Logo />
-		</div>
-	)
+  const location = useLocation()
+  const path = location.pathname
+  return (
+    <>
+      {path !== "/order-confirm" && path !== "/feedback" && (
+        <div className="my-5 flex w-[80rem] items-center justify-between">
+          <BackButton />
+          {path !== "/customize" &&
+          path !== "/cart" &&
+          path !== "/checkout" &&
+          path !== "/card-details" ? (
+            <Menu />
+          ) : (
+            <></>
+          )}
+          <Logo />
+        </div>
+      )}
+    </>
+  )
 }
 
 export default Navbar
