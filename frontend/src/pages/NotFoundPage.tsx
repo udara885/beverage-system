60d568795d32456d855b1dfd53b70fc5afdec70f
Udara Lakshan <ulakshan061@gmail.com>
2025-03-05 18:06:44 +0530

    Add NotFoundPage component for handling 404 errors

---
+++ frontend/src/pages/NotFoundPage.tsx
@@ -0,0 +1,9 @@
+const NotFoundPage = () => {
+  return (
+    <div className="font-poppins flex h-full items-center justify-center text-4xl font-bold">
+      Sorry! Page Not Found.😢
+    </div>
+  )
+}
+
+export default NotFoundPage
