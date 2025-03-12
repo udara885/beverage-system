e81d0d56c38b45f448dccc89d8068f7f377f55f0
Udara Lakshan <ulakshan061@gmail.com>
2025-03-12 23:49:54 +0530

    Refactor MenuContent component to accept beverages as props and render BeverageCard for each beverage

--- frontend/src/components/MenuContent.tsx
+++ frontend/src/components/MenuContent.tsx
@@ -1,12 +1,11 @@
+import { Beverage } from "../types/types"
 import BeverageCard from "./BeverageCard"
 
-const MenuContent = () => {
+const MenuContent = ({ beverages }: { beverages: Beverage[] }) => {
   return (
     <div className="no-scrollbar mx-7 mb-7 max-h-[calc(100vh-30vh)] overflow-y-scroll">
-      <BeverageCard />
-      <BeverageCard />
-      <BeverageCard />
-      <BeverageCard />
+      {beverages.length !== 0 &&
+        beverages.map((beverage) => <BeverageCard beverage={beverage} />)}
       <hr />
     </div>
   )
