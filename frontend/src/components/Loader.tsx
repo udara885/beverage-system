b41a819de65587c99e1766094a1342eddd1472bc
Udara Lakshan <ulakshan061@gmail.com>
2025-03-12 23:48:58 +0530

    Add Loader component for displaying loading state

---
+++ frontend/src/components/Loader.tsx
@@ -0,0 +1,9 @@
+const Loader = () => {
+  return (
+    <div className="mt-50 flex h-screen flex-col">
+      <div className="mx-auto h-16 w-16 animate-spin rounded-full border-4 border-dashed border-orange-400"></div>
+    </div>
+  )
+}
+
+export default Loader
