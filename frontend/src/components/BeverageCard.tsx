1d015c02eb1cfa813a55fe63fc0f5e5e3390fcb0
Udara Lakshan <ulakshan061@gmail.com>
2025-03-12 23:50:08 +0530

    Refactor BeverageCard component to accept beverage data as props and display dynamic content

--- frontend/src/components/BeverageCard.tsx
+++ frontend/src/components/BeverageCard.tsx
@@ -1,22 +1,22 @@
-import { Link } from "react-router-dom";
+import { Link } from "react-router-dom"
+import { Beverage } from "../types/types"
 
-const BeverageCard = () => {
+const BeverageCard = ({ beverage }: { beverage: Beverage }) => {
   return (
     <div>
       <hr />
       <div className="mt-5 flex gap-5">
         <img
-          src="espresso.png"
+          src={beverage.image}
           alt=""
           className="h-30 w-30 rounded-full bg-no-repeat object-cover"
           loading="lazy"
         />
         <div className="font-poppins flex flex-col">
-          <h1 className="text-[1.75rem] font-semibold">Espresso - $3.00</h1>
-          <p className="text-[1.375rem]">
-            A rich, concentrated coffee brewed by forcing a small amount of
-            nearly boiling water through finely-ground coffee beans.
-          </p>
+          <h1 className="text-[1.75rem] font-semibold">
+            {beverage.name} - ${beverage.price}
+          </h1>
+          <p className="text-[1.375rem]">{beverage.description}</p>
           <Link
             to={"/customize"}
             className="mx-3 mb-3 self-end rounded-lg bg-orange-400 px-3 text-center text-lg font-semibold text-white"
@@ -26,7 +26,7 @@ const BeverageCard = () => {
         </div>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default BeverageCard;
+export default BeverageCard
