77502df73e9d8386b2dc6bc0842b5a6805ad0c13
Udara Lakshan <ulakshan061@gmail.com>
2025-03-16 23:38:26 +0530

    Add delete functionality to AdminPage with user feedback

--- frontend/src/pages/AdminPage.tsx
+++ frontend/src/pages/AdminPage.tsx
@@ -2,9 +2,10 @@ import { useEffect, useState } from "react"
 import AdminBeverageCard from "../components/AdminBeverageCard"
 import { useBeverageStore } from "../store/beverage"
 import Loader from "../components/Loader"
+import toast from "react-hot-toast"
 
 const AdminPage = () => {
-  const { beverages, getBeverages } = useBeverageStore()
+  const { beverages, getBeverages, deleteBeverage } = useBeverageStore()
 
   const [loading, setLoading] = useState(true)
 
@@ -17,6 +18,22 @@ const AdminPage = () => {
     fetchBeverages()
   }, [getBeverages])
 
+  const handleDelete = async (id: string | undefined): Promise<void> => {
+    if (!id) {
+      throw new Error("id is undefined")
+    }
+    const { success, message } = await deleteBeverage(id)
+    if (!success) {
+      toast.error(message)
+    } else {
+      toast.success(message)
+      const updatedBeverages = beverages.filter(
+        (beverage) => beverage._id !== id,
+      )
+      useBeverageStore.setState({ beverages: updatedBeverages })
+    }
+  }
+
   return (
     <div className="font-poppins flex w-full justify-center">
       {loading ? (
@@ -24,8 +41,12 @@ const AdminPage = () => {
       ) : (
         <div className="no-scrollbar flex h-[calc(100vh-13vh)] w-[90%] flex-wrap items-center justify-center gap-10 overflow-y-scroll py-4">
           {beverages.length !== 0 ? (
-            beverages.map((beverage) => (
-              <AdminBeverageCard beverage={beverage} />
+            beverages.map((beverage, index) => (
+              <AdminBeverageCard
+                key={index}
+                beverage={beverage}
+                handleDelete={() => handleDelete(beverage._id)}
+              />
             ))
           ) : (
             <p className="font-poppins flex items-center justify-center text-4xl font-bold">
