09fb5d60442624d9601801095d50eae35e8bb4b1
Udara Lakshan <ulakshan061@gmail.com>
2025-03-18 18:45:52 +0530

    Implement filtering of beverages on MenuPage and display message for no results

--- frontend/src/pages/MenuPage.tsx
+++ frontend/src/pages/MenuPage.tsx
@@ -3,6 +3,7 @@ import MenuContent from "../components/MenuContent"
 import { useBeverageStore } from "../store/beverage"
 import { useEffect, useState } from "react"
 import Loader from "../components/Loader"
+import { Beverage } from "../types/types"
 
 const MenuPage = () => {
   const location = useLocation()
@@ -15,6 +16,8 @@ const MenuPage = () => {
 
   const [loading, setLoading] = useState(true)
 
+  const [filteredBeverages, setFilteredBeverages] = useState<Beverage[]>([])
+
   useEffect(() => {
     const fetchBeverages = async () => {
       setLoading(true)
@@ -24,39 +27,55 @@ const MenuPage = () => {
     fetchBeverages()
   }, [getBeverages])
 
+  useEffect(() => {
+    setFilteredBeverages(
+      str === ""
+        ? beverages.filter((beverage) => beverage.category === "coffee")
+        : beverages.filter((beverage) => beverage.category === str),
+    )
+  }, [str, beverages])
+
   return (
     <div>
       {loading ? (
         <Loader />
       ) : (
         <>
-          <h1 className="font-poppins mx-15 my-6 text-5xl font-medium">
-            {str.charAt(0).toUpperCase() + str.slice(1) === "Bubble-tea"
-              ? "Bubble Tea"
-              : str.charAt(0).toUpperCase() + str.slice(1) === ""
-                ? "Coffee"
-                : str.charAt(0).toUpperCase() + str.slice(1)}
-          </h1>
-          <div className="flex w-screen justify-between px-8">
-            <div className="h-10 w-2/3">
-              <MenuContent beverages={beverages} />
-            </div>
-            <div className="w-1/3">
-              <img
-                src={
-                  path === "/coffee" || path === "/"
-                    ? "coffee.png"
-                    : path === "/shake"
-                      ? "shake.png"
-                      : path === "/tea"
-                        ? "tea.png"
-                        : "bubble-tea.png"
-                }
-                alt=""
-                className="-mt-31 bg-no-repeat object-cover"
-              />
-            </div>
-          </div>
+          {filteredBeverages.length !== 0 ? (
+            <>
+              <h1 className="font-poppins mx-15 my-6 text-5xl font-medium">
+                {str.charAt(0).toUpperCase() + str.slice(1) === "Bubble-tea"
+                  ? "Bubble Tea"
+                  : str.charAt(0).toUpperCase() + str.slice(1) === ""
+                    ? "Coffee"
+                    : str.charAt(0).toUpperCase() + str.slice(1)}
+              </h1>
+              <div className="flex w-screen justify-between px-8">
+                <div className="h-10 w-2/3">
+                  <MenuContent beverages={filteredBeverages} />
+                </div>
+                <div className="w-1/3">
+                  <img
+                    src={
+                      path === "/coffee" || path === "/"
+                        ? "coffee.png"
+                        : path === "/shake"
+                          ? "shake.png"
+                          : path === "/tea"
+                            ? "tea.png"
+                            : "bubble-tea.png"
+                    }
+                    alt=""
+                    className="-mt-31 bg-no-repeat object-cover"
+                  />
+                </div>
+              </div>
+            </>
+          ) : (
+            <p className="font-poppins mt-50 flex items-center justify-center text-4xl font-bold">
+              No Beverages Found.😢
+            </p>
+          )}
         </>
       )}{" "}
     </div>
