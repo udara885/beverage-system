af31fbcf95c2e6f9d9c80c9510683329ebbc8b5d
Udara Lakshan <ulakshan061@gmail.com>
2025-03-19 21:51:12 +0530

    Refactor CartCard component to accept cartItem and onRemove props, enhancing dynamic rendering and customization display

--- frontend/src/components/CartCard.tsx
+++ frontend/src/components/CartCard.tsx
@@ -1,44 +1,72 @@
 import { Settings, Trash } from "lucide-react"
-import { useLocation } from "react-router-dom"
+import { Link, useLocation } from "react-router-dom"
+import { CartItem } from "../types/types"
 
-const CartCard = () => {
+const CartCard = ({
+  cartItem,
+  onRemove,
+}: {
+  cartItem: CartItem
+  onRemove?: (id: string) => void
+}) => {
   const location = useLocation()
+
   const path = location.pathname
+
   return (
-    <div className="flex w-full items-center justify-around gap-5 rounded-lg px-6 shadow-[0px_4px_30.8px_0px_rgba(0,0,0,0.25)]">
-      <img src="cartCard.png" className="my-3 h-40 w-40 rounded-full" alt="" />
-      <div className="font-poppins">
-        <h2 className="text-2xl font-semibold">Berry Blast Smoothie</h2>
-        {path === "/cart" ? (
-          <>
-            <div className="flex gap-1 text-lg font-medium">
-              <h3>customized - </h3>
-              <div>
-                <h3>Whole Milk</h3>
-                <h3>Vanilla</h3>
+    <div className="relative mb-5 flex w-full gap-30 rounded-lg px-5 py-3 shadow-sm">
+      <div className="flex items-center gap-5">
+        <img
+          src={cartItem.image}
+          className="my-3 h-30 w-30 rounded-full object-cover"
+          alt=""
+        />
+        <div className="font-poppins">
+          <h2 className="text-2xl font-semibold">{cartItem.name}</h2>
+          {path === "/cart" ? (
+            <>
+              {cartItem.customization && (
+                <div className="flex gap-1 text-lg font-medium">
+                  <h3>customized - </h3>
+                  <div>
+                    <h3 className="capitalize">
+                      {cartItem.customization.milk?.split("-").join(" ")}
+                    </h3>
+                    <h3 className="capitalize">
+                      {cartItem.customization.flavours}
+                    </h3>
+                    <h3 className="capitalize">
+                      {cartItem.customization.sweeteners}
+                    </h3>
+                  </div>
+                </div>
+              )}
+              <div className="mt-5 flex w-40 items-center justify-around rounded-lg border-2">
+                <button className="cursor-pointer">-</button>
+                <span className="font-bold">{cartItem.quantity}</span>
+                <button className="cursor-pointer">+</button>
               </div>
-            </div>
-            <div className="mt-5 flex w-1/2 items-center justify-around rounded-lg border-2">
-              <button className="cursor-pointer">-</button>
-              <span className="font-bold">1</span>
-              <button className="cursor-pointer">+</button>
-            </div>
-          </>
-        ) : (
-          <p className="text-justify text-lg">
-            A refreshing blend of strawberries, blueberries, and raspberries
-            mixed with yogurt and a splash of apple juice for a fruity delight.
-          </p>
-        )}
+            </>
+          ) : (
+            <p className="text-justify text-lg">{cartItem.description}</p>
+          )}
+        </div>
       </div>
       {path === "/cart" && (
-        <div className="flex flex-col gap-2 self-end pb-5">
-          <button className="cursor-pointer rounded-lg bg-orange-400 p-1 text-white">
+        <div className="absolute right-5 flex flex-col gap-2 self-end pb-5">
+          <button
+            className="cursor-pointer rounded-lg bg-orange-400 p-1 text-white"
+            onClick={() => cartItem._id && onRemove?.(cartItem._id)}
+          >
             <Trash />
           </button>
-          <button className="cursor-pointer rounded-lg bg-orange-400 p-1 text-white">
+          <Link
+            to={"/customize"}
+            state={{ cartItem }}
+            className="cursor-pointer rounded-lg bg-orange-400 p-1 text-white"
+          >
             <Settings />
-          </button>
+          </Link>
         </div>
       )}
     </div>
