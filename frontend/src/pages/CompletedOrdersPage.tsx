396798b760058578f5cf3dfdebc3aadb4122e57a
Udara Lakshan <ulakshan061@gmail.com>
2025-03-26 20:26:41 +0530

    Implement loading state and order filtering in CompletedOrdersPage component

--- frontend/src/pages/CompletedOrdersPage.tsx
+++ frontend/src/pages/CompletedOrdersPage.tsx
@@ -1,7 +1,25 @@
 import { Search } from "lucide-react"
 import KitchenCard from "../components/KitchenCard"
+import { useOrderStore } from "../store/order"
+import { useEffect, useState } from "react"
+import Loader from "../components/Loader"
 
 const CompletedOrdersPage = () => {
+  const { orders, getOrders } = useOrderStore()
+
+  const [loading, setLoading] = useState(true)
+
+  const filteredOrders = orders.filter((order) => order.status === "Completed")
+
+  useEffect(() => {
+    const fetchOrders = async () => {
+      setLoading(true)
+      await getOrders()
+      setLoading(false)
+    }
+    fetchOrders()
+  }, [getOrders])
+
   return (
     <div className="font-poppins flex w-full justify-center">
       <div className="w-[90%]">
@@ -13,9 +31,21 @@ const CompletedOrdersPage = () => {
           />
           <Search className="absolute top-3 right-80" />
         </div>
-        <div className="no-scrollbar h-[calc(100vh-30vh)] overflow-y-scroll">
-          <KitchenCard />
-        </div>
+        {loading ? (
+          <Loader />
+        ) : (
+          <div className="no-scrollbar h-[calc(100vh-30vh)] overflow-y-scroll">
+            {filteredOrders.length !== 0 ? (
+              filteredOrders.map((order, index) => (
+                <KitchenCard order={order} index={index + 1} />
+              ))
+            ) : (
+              <p className="font-poppins mt-50 flex items-center justify-center text-4xl font-bold">
+                No Completed Orders.😢
+              </p>
+            )}
+          </div>
+        )}
       </div>
     </div>
   )
