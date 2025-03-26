aab032421b32b45011975954e834d3269659dfc5
Udara Lakshan <ulakshan061@gmail.com>
2025-03-26 20:27:46 +0530

    Refactor KitchenCard component to display order items and their customizations

--- frontend/src/components/KitchenCard.tsx
+++ frontend/src/components/KitchenCard.tsx
@@ -1,50 +1,55 @@
 import { BookOpen } from "lucide-react"
 import { Link, useLocation } from "react-router-dom"
+import { Order } from "../types/types"
 
-const KitchenCard = () => {
+const KitchenCard = ({ order, index }: { order: Order; index: number }) => {
   const location = useLocation()
   const path = location.pathname
   return (
     <div className="flex items-center justify-between border-t py-5">
-      <div className="flex gap-5">
-        <img
-          src="/espresso.png"
-          alt=""
-          className="h-30 w-30 rounded-full object-contain"
-        />
-        <div>
-          <h1 className="text-[1.75rem] font-semibold">Espresso</h1>
-          <div className="flex gap-1">
-            <span>Customized - </span>
-            <div className="flex flex-col">
-              <span>Whole Milk</span>
-              <span>Vanilla</span>
+      <div className="flex flex-col gap-5">
+        {order.items.map((item) => (
+          <div className="flex gap-5" key={item._id}>
+            <img
+              src={item.image}
+              alt={item.name}
+              className="h-30 w-30 rounded-full object-cover"
+            />
+            <div>
+              <h1 className="text-[1.75rem] font-semibold">{item.name}</h1>
+              {item.customization && (
+                <div className="flex gap-1">
+                  <span>Customized - </span>
+                  <div className="flex flex-col">
+                    <span>{item.customization?.milk}</span>
+                    <span>{item.customization?.flavours}</span>
+                    <span>{item.customization?.sweeteners}</span>
+                  </div>
+                </div>
+              )}
+              <span>Time - 15min</span>
             </div>
           </div>
-          <span>Time - 15min</span>
-        </div>
+        ))}
       </div>
-
-      <select
-        name=""
-        id=""
-        className="h-[60px] w-[280px] rounded-lg px-5 text-lg text-gray-600 shadow-[0_4px_14px_0_rgba(0,0,0,0.10)]"
-      >
-        <option>Process</option>
-        <option value="new-order">New Order</option>
-        <option value="processing">Processing</option>
-        <option value="completed">Completed</option>
-      </select>
-      {path !== "/kitchen/completed-orders" && (
-        <Link
-          to={"/kitchen/instructions"}
-          className="rounded-lg bg-orange-400 px-6 py-2"
-        >
-          <BookOpen size={30} fill="white" stroke="white" />
-        </Link>
-      )}
-      <div className="flex h-[90px] w-[90px] items-center justify-center rounded-full border">
-        <span className="text-3xl font-semibold">1</span>
+      <div className="flex w-[60%] items-center justify-between">
+        <select className="mr-10 h-[60px] w-[280px] rounded-lg px-5 text-lg text-gray-600 shadow-[0_4px_14px_0_rgba(0,0,0,0.10)]">
+          <option>Process</option>
+          <option value="new-order">New Order</option>
+          <option value="processing">Processing</option>
+          <option value="completed">Completed</option>
+        </select>
+        {path !== "/kitchen/completed-orders" && (
+          <Link
+            to={"/kitchen/instructions"}
+            className="rounded-lg bg-orange-400 px-6 py-2"
+          >
+            <BookOpen size={30} fill="white" stroke="white" />
+          </Link>
+        )}
+        <div className="flex h-[90px] w-[90px] items-center justify-center rounded-full border">
+          <span className="text-3xl font-semibold">{index}</span>
+        </div>
       </div>
     </div>
   )
