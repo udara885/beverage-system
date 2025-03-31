b944610c3f22445b7e51cc94e52a21f8482538b9
Udara Lakshan <ulakshan061@gmail.com>
2025-03-31 12:55:41 +0530

    Refactor InstructionPage to dynamically display order items and their customizations

--- frontend/src/pages/InstructionPage.tsx
+++ frontend/src/pages/InstructionPage.tsx
@@ -1,4 +1,11 @@
+import { useLocation } from "react-router-dom"
+import { Order } from "../types/types"
+
 const InstructionPage = () => {
+  const location = useLocation()
+
+  const order: Order = location.state.order
+
   return (
     <div className="font-poppins flex w-full justify-center">
       <div className="no-scrollbar flex h-[calc(100vh-20vh)] w-[90%] flex-col overflow-y-scroll">
@@ -8,46 +15,45 @@ const InstructionPage = () => {
             <span className="text-3xl font-semibold">1</span>
           </div>
         </div>
-        <div className="mt-2 flex items-center justify-between border-b pb-2">
-          <div className="flex items-center gap-10">
-            <img
-              src="/espresso.png"
-              alt=""
-              className="h-44 w-44 rounded-full object-contain"
-            />
-            <h2 className="text-[1.75rem] font-bold">Espresso</h2>
-          </div>
-          <select
-            name=""
-            id=""
-            className="h-[60px] w-[280px] rounded-lg px-5 text-lg text-gray-600 shadow-[0_4px_14px_0_rgba(0,0,0,0.10)]"
-          >
-            <option>Process</option>
-            <option value="new-order">New Order</option>
-            <option value="processing">Processing</option>
-            <option value="completed">Completed</option>
-          </select>
-        </div>
-        <div className="border-b py-5">
-          <h2 className="text-2xl font-semibold">Customize</h2>
-          <span>Whole Milk</span>
-          <br />
-          <span>Vanilla</span>
-        </div>
-        <div className="border-b py-5">
-          <span>Item - 2</span>
-          <br />
-          <span>Time - 15min</span>
-        </div>
-        <div className="py-5">
-          <h2 className="text-2xl font-semibold">Instructions</h2>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis
-            harum assumenda vel mollitia sapiente quidem quam in aliquam
-            ratione, temporibus atque ipsa unde esse similique eligendi
-            provident! Et, dolore nemo.
-          </p>
-        </div>
+        {order.items.map((item) => (
+          <>
+            <div className="mt-2 flex items-center justify-between border-b pb-2">
+              <div className="flex items-center gap-10">
+                <img
+                  src={item.image}
+                  alt=""
+                  className="h-44 w-44 rounded-full object-cover"
+                />
+                <h2 className="text-[1.75rem] font-bold">{item.name}</h2>
+              </div>
+              <select
+                name=""
+                id=""
+                className="h-[60px] w-[280px] rounded-lg px-5 text-lg text-gray-600 shadow-[0_4px_14px_0_rgba(0,0,0,0.10)]"
+              >
+                <option>Process</option>
+                <option value="new-order">New Order</option>
+                <option value="processing">Processing</option>
+                <option value="completed">Completed</option>
+              </select>
+            </div>
+            <div className="border-b py-5">
+              <h2 className="text-2xl font-semibold">Customize</h2>
+              <span>{item.customization?.milk}</span>
+              <span>{item.customization?.flavours}</span>
+              <span>{item.customization?.sweeteners}</span>
+            </div>
+            <div className="border-b py-5">
+              <span>Item - {item.quantity}</span>
+              <br />
+              <span>Time - 15min</span>
+            </div>
+            <div className="py-5">
+              <h2 className="text-2xl font-semibold">Instructions</h2>
+              <p>{item.customization?.specialInstructions}</p>
+            </div>
+          </>
+        ))}
       </div>
     </div>
   )
